0feb0bd8c8acef3fc7db48077ced0a877b713597
Ravi Patel <63658662+ravipatel0508@users.noreply.github.com>
2022-06-05 17:25:15 +0530

    code optimization

--- challenge1/lib/homepage.dart
+++ challenge1/lib/homepage.dart
@@ -14,112 +14,106 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(25.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Spacer(),
-            _header(),
-            const AppTextField(
-              hintText: "Email",
-            ),
-            const AppTextField(
-              hintText: "Password",
-            ),
-            Row(
-              children: [
-                Container(
-                  margin: const EdgeInsets.only(
-                    right: 8.0,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(25.0),
+          child: ListView(
+            shrinkWrap: true,
+            children: [
+              _header(),
+              const AppTextField(
+                hintText: "Email",
+              ),
+              const AppTextField(
+                hintText: "Password",
+              ),
+              Row(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(
+                      right: 8.0,
+                    ),
+                    height: 18,
+                    width: 18,
+                    child: Checkbox(
+                      shape: const CircleBorder(),
+                      value: isSelect,
+                      onChanged: (value) {
+                        setState(() {
+                          isSelect = value;
+                        });
+                      },
+                      activeColor: Colors.black,
+                    ),
                   ),
-                  height: 18,
-                  width: 18,
-                  child: Checkbox(
-                    shape: const CircleBorder(),
-                    value: isSelect,
-                    onChanged: (value) {
-                      setState(() {
-                        isSelect = value;
-                      });
-                    },
-                    activeColor: Colors.black,
+                  const Text(
+                    "Remember me",
+                    style: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontSize: 13,
+                      fontWeight: FontWeight.w500,
+                    ),
                   ),
-                ),
-                const Text(
-                  "Remember me",
-                  style: TextStyle(
-                    fontFamily: 'Montserrat',
-                    fontSize: 13,
-                    fontWeight: FontWeight.w500,
+                  const Spacer(),
+                  const Text(
+                    "Forgot password?",
+                    style: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontSize: 13,
+                      fontWeight: FontWeight.w500,
+                      color: Color(0xFFC01C1C),
+                    ),
                   ),
-                ),
-                const Spacer(),
-                const Text(
-                  "Forgot password?",
-                  style: TextStyle(
-                    fontFamily: 'Montserrat',
-                    fontSize: 13,
-                    fontWeight: FontWeight.w500,
-                    color: Color(0xFFC01C1C),
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 50),
-            _footer(),
-            const Spacer()
-          ],
+                ],
+              ),
+              const SizedBox(height: 50),
+              _footer(),
+            ],
+          ),
         ),
       ),
     );
   }
 
   Widget _header() {
-    return const Expanded(
-      child: Align(
-        alignment: Alignment.centerLeft,
-        child: Text(
-          "Welcome back",
-          style: TextStyle(
-            fontSize: 26.0,
-            fontFamily: 'Montserrat',
-            fontWeight: FontWeight.w600,
-          ),
+    return const Align(
+      alignment: Alignment.centerLeft,
+      child: Text(
+        "Welcome back",
+        style: TextStyle(
+          fontSize: 26.0,
+          fontFamily: 'Montserrat',
+          fontWeight: FontWeight.w600,
         ),
       ),
     );
   }
 
   Widget _footer() {
-    return Expanded(
-      flex: 1,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          const Text(
-            "Sign In",
-            style: TextStyle(
-              fontSize: 22.0,
-              fontFamily: 'Montserrat',
-              fontWeight: FontWeight.w700,
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        const Text(
+          "Sign In",
+          style: TextStyle(
+            fontSize: 22.0,
+            fontFamily: 'Montserrat',
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+        Container(
+          margin: const EdgeInsets.only(left: 8.0),
+          child: const CircleAvatar(
+            minRadius: 24.0,
+            backgroundColor: Colors.black,
+            child: Icon(
+              Icons.arrow_forward_outlined,
+              size: 30,
+              color: Colors.white,
             ),
           ),
-          Container(
-            margin: const EdgeInsets.only(left: 8.0),
-            child: const CircleAvatar(
-              minRadius: 24.0,
-              backgroundColor: Colors.black,
-              child: Icon(
-                Icons.arrow_forward_outlined,
-                size: 30,
-                color: Colors.white,
-              ),
-            ),
-          ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
